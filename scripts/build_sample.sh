44ebb2d4d3400e859244c82a512ecfb30b44508f
elizasapir <eliza.koren@logmein.com>
2018-07-17 17:07:40 +0300

    global sample script runner

---
+++ scripts/build_sample.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#exit on failure and pipeline used to receive the exit code from xcpretty
+set -eo pipefail
+
+#ARGUMENTS
+FOLDER_NAME=$1
+SUB_FOLDER_NAME=$2
+WORKSPACE_NAME=$3
+
+echo arguments: $*
+
+echo cd to $FOLDER_NAME
+cd $FOLDER_NAME
+if [ $SUB_FOLDER_NAME != "" ]; then
+echo cd to $SUB_FOLDER_NAME
+cd $SUB_FOLDER_NAME
+fi
+
+pod update --repo-update
+
+echo -e "\nbuild target for $FOLDER_NAME\n"
+xcodebuild -scheme $WORKSPACE_NAME -workspace $WORKSPACE_NAME.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.1' | tee xcodebuild.log | xcpretty
