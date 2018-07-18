1c795b862717826d346f7e0c4dd49aafcb53becd
ElizaSapir <eliza.koren@logmein.com>
2018-07-18 15:48:48 +0300

    Update build_sample.sh

--- scripts/build_sample.sh
+++ scripts/build_sample.sh
@@ -5,7 +5,10 @@ FOLDER_NAME=$1
 SUB_FOLDER_NAME=$2
 WORKSPACE_NAME=$3
 
-sudo dos2unix $FOLDER_NAME
+for i in *; do 
+if [[ -f $i ]]; 
+then sed -i 's/\r$//' "$i"; 
+fi; done
 
 # echo cd to $FOLDER_NAME
 cd $FOLDER_NAME
